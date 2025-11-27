eaaa6a471e7ed9b39de2408806d4dd8312cc0ac4
Junyoung <compilevoid@gmail.com>
2025-11-27 15:30:03 -0800

    Update page.tsx

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,5 +1,15 @@
 export default function LoginPage() {
   return (
-    <div>home</div>
+    <form class="flex flex-col p-4">
+      <div class="">
+        <label>email</label>
+        <input placeholder="email" />
+      </div>
+      <div class="">
+        <label>password</label>
+        <input placeholder="password" />
+      </div>
+      <button class="bg-blue-500 rounded-md">login</button>
+    </form>
   );
 }
